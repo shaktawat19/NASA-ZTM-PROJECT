b22db5da1c45affa75c2c3779b0fd091e10d8566
shaktawat19 <harshshaktawat.tech@gmail.com>
2023-08-08 12:24:41 +0530

    logging requests with morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const path = require('path');
+const path = require("path");
+const morgan = require("morgan");
 
 const app = express();
 
@@ -13,12 +14,14 @@ app.use(
   })
 );
 
+app.use(morgan('combined')); // logging requests with morgan.
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-})
 
 app.use(planetsRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
