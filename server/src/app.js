ee07b944ea291d458a787031dd44f73ec81353e0
shaktawat19 <harshshaktawat.tech@gmail.com>
2023-08-08 11:38:05 +0530

    Serving ReactJS build in Production

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require('path');
 
 const app = express();
 
@@ -13,6 +14,11 @@ app.use(
 );
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+})
+
 app.use(planetsRouter);
 
 module.exports = app;
